4c3829c1e174dc788da537d85027f6dc39378cb2
Humus Digital <eduardo@vstream.ie>
2017-09-16 13:13:50 -0300

    hacer magia con gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,13 @@
-var gulp = require ('gulp');
-gulp.task('default', function(){});
+var gulp = require('gulp'),
+    inlineCss = require('gulp-inline-css');
+
+gulp.task('default', function() {
+    return gulp.src('./*.html')
+        .pipe(inlineCss({
+            	applyStyleTags: true,
+            	applyLinkTags: true,
+            	removeStyleTags: true,
+            	removeLinkTags: true
+        }))
+        .pipe(gulp.dest('build/'));
+});
