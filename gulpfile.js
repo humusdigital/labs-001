75909dbbc6309ac04f095124a05d5c3dad1ff903
Martin <martincolazo.mc@gmail.com>
2017-09-20 20:16:17 -0300

    codigo html minimizado

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,11 @@ gulp.task('default', function() {
         }))
         .pipe(gulp.dest('build/'));
 });
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('minify', function() {
+  return gulp.src('src/index.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
